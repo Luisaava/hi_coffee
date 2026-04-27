6a46452c7db52ac61f32dc0faad7dcd9b66bcb62
Luisaava <luisaava59@gmail.com>
2026-04-27 15:56:39 -0300

    coisas

--- hicoffee/src/App.jsx
+++ hicoffee/src/App.jsx
@@ -1,13 +1,13 @@
 import 'styles.css'
 import React from 'react'
 
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Hub from './components/Hub';
-import Menu from './components/Menu';
-import About from './components/About';
-import Experience from './components/Experience';
-import Footer from './components/Footer';
+import {Header} from './components/Header';
+import {Hero} from './components/Hero';
+import {Hub} from './components/Hub';
+import {Menu} from './components/Menu';
+import {About} from './components/About';
+import {Experience} from './components/Experience';
+import {Footer} from './components/Footer';
 
 function App() {
   return (
